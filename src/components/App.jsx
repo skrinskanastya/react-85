2092d49149f9619d4989c7d428b06e7f4b0789c9
skrinskanastya <nastiolla@gmail.com>
2023-08-16 21:18:01 +0300

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { QuizForm } from './QuizForm';
+import { SearchBar } from './SearchBar';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <QuizForm />
+      <SearchBar />
     </div>
   );
 };
---
+++ src/components/SearchBar.js
@@ -0,0 +1,8 @@
+export const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Topic filter"></input>
+      <select></select>
+    </div>
+  );
+};
